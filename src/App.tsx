3ab11c69c7331362c56e5f49de41505a15ed776d
Yevhenii <sharovskiyevgeniy@gmail.com>
2022-05-04 01:52:13 +0300

    PMA-1 feat(project): add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import Stack from '@mui/material/Stack';
+
+import styles from './styles.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className={styles.app}>
+      <header className={styles.appHeader}>
+        <Stack direction="row" spacing={2}>
+          <Button variant="outlined" startIcon={<DeleteIcon />}>
+            Delete
+          </Button>
+          <Button variant="contained" endIcon={<SendIcon />}>
+            Send
+          </Button>
+        </Stack>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
