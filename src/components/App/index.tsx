0bcb38cd9cc7ddb6b1c1e7e6f9715f6c5f1aee1c
Yevhenii <sharovskiyevgeniy@gmail.com>
2022-05-04 04:52:01 +0300

    PMA-1 refactor(project): delete unnecessary files

---
+++ src/components/App/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss';
+
+export function App() {
+  return (
+    <>
+      <h1 className={styles.testClass}>project-management-app</h1>
+    </>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+
+import { App } from './components/App';
+
 import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
