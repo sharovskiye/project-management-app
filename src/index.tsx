6743f8398a98ed03625932179c4f3ed6f22c1068
Yevhenii <sharovskiyevgeniy@gmail.com>
2022-05-04 04:09:13 +0300

    PMA-1 fix(project): delete log

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
-console.log();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
