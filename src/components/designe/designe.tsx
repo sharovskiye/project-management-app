17259b14c60d0eb3cd6d66a09543837f4358daed
novikova.n_qschool <nadeysha23@gmail.com>
2022-05-05 19:18:42 +0400

    PMA-13 feat(designe): add variant with dark theme

--- src/components/designe/designe.tsx
+++ src/components/designe/designe.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Select, MenuItem, SelectChangeEvent, FormControl, Link } from '@mui/material';
+import { Select, MenuItem, SelectChangeEvent, FormControl, Link, Button } from '@mui/material';
 import { useState } from 'react';
 import style from './style.module.scss';
 
@@ -12,26 +12,83 @@ export const Designe = () => {
 
   return (
     <>
-      <header className={`${style.wrapper__light} ${style.header}`}>
-        <div className={`${style.container} ${style.container__big}`}>
-          <div className={style.link}>OUR LOGO</div>
-          <Link className={style.link}>Create new board</Link>
-          <Link className={style.link}>Log Out</Link>
-          <Link className={style.link}>Edit Prifile</Link>
-          <FormControl className={style.form}>
-            <Select className={style.form_select} value={age} onChange={handleChange}>
-              <MenuItem value={'RU'}>RU</MenuItem>
-              <MenuItem value={'EN'}>EN</MenuItem>
-            </Select>
-          </FormControl>
-        </div>
-      </header>
-      <main className={`${style.wrapper__light} ${style.main}`}>
-        <div className={`${style.container} ${style.container__medium}`}></div>
-      </main>
-      <footer className={`${style.wrapper__dark} ${style.footer}`}>
-        <div className={`${style.container} ${style.container__medium}`}></div>
-      </footer>
+      <section>
+        <header className={`${style.lightTheme}`}>
+          <div className={`${style.container} ${style.container__big}  ${style.header}`}>
+            <div className={`${style.header_link} ${style.link}`}>HEADER</div>
+            <Link className={`${style.header_link} ${style.link}`}>HEADER LINK</Link>
+            <Button variant="outlined" className={`${style.header_button} ${style.button}`}>
+              Header button
+            </Button>
+            <FormControl className={style.form}>
+              <Select className={style.form_select} value={age} onChange={handleChange}>
+                <MenuItem value={'RU'}>RU</MenuItem>
+                <MenuItem value={'EN'}>EN</MenuItem>
+              </Select>
+            </FormControl>
+            <span className={`${style.line}`}></span>
+          </div>
+        </header>
+
+        <main className={`${style.lightTheme}`}>
+          <div className={`${style.container} ${style.container__medium}  ${style.main}`}></div>
+        </main>
+
+        <footer className={`${style.darkTheme}`}>
+          <div className={`${style.container} ${style.container__medium}  ${style.footer}`}>
+            <div className={style.footer_githubBlock}>
+              <Link className={`${style.footer_link} ${style.link}`}>GITHUB</Link>
+              <Link className={`${style.footer_link} ${style.link}`}>GITHUB</Link>
+              <Link className={`${style.footer_link} ${style.link}`}>GITHUB</Link>
+            </div>
+            <div className={style.footer_year}>
+              <p>2022</p>
+            </div>
+            <div className={style.footer_rssCourse}>
+              <Link className={`${style.footer_link} ${style.link}`}>RSS</Link>
+            </div>
+          </div>
+        </footer>
+      </section>
+
+      <section>
+        <header className={`${style.darkTheme}`}>
+          <div className={`${style.container} ${style.container__big}  ${style.header}`}>
+            <div className={`${style.header_link} ${style.link}`}>HEADER</div>
+            <Link className={`${style.header_link} ${style.link}`}>HEADER LINK</Link>
+            <Button variant="outlined" className={`${style.header_button} ${style.button}`}>
+              Header button
+            </Button>
+            <FormControl className={style.form}>
+              <Select className={style.form_select} value={age} onChange={handleChange}>
+                <MenuItem value={'RU'}>RU</MenuItem>
+                <MenuItem value={'EN'}>EN</MenuItem>
+              </Select>
+            </FormControl>
+            <span className={`${style.line}`}></span>
+          </div>
+        </header>
+
+        <main className={`${style.darkTheme}`}>
+          <div className={`${style.container} ${style.container__medium}  ${style.main}`}></div>
+        </main>
+
+        <footer className={`${style.lightTheme}`}>
+          <div className={`${style.container} ${style.container__medium}  ${style.footer}`}>
+            <div className={style.footer_githubBlock}>
+              <Link className={`${style.footer_link} ${style.link}`}>GITHUB</Link>
+              <Link className={`${style.footer_link} ${style.link}`}>GITHUB</Link>
+              <Link className={`${style.footer_link} ${style.link}`}>GITHUB</Link>
+            </div>
+            <div className={style.footer_year}>
+              <p>2022</p>
+            </div>
+            <div className={style.footer_rssCourse}>
+              <Link className={`${style.footer_link} ${style.link}`}>RSS</Link>
+            </div>
+          </div>
+        </footer>
+      </section>
     </>
   );
 };
