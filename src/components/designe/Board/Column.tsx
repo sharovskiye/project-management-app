4ad67ef209e939c2055b75911a4bcb48114dcea2
novikova.n_qschool <nadeysha23@gmail.com>
2022-05-07 23:04:16 +0400

    PMA-13 feat(designe): add modal window

--- src/components/designe/Board/Column.tsx
+++ src/components/designe/Board/Column.tsx
@@ -2,35 +2,52 @@ import { Tooltip } from '@mui/material';
 import { useCallback, useState } from 'react';
 import { CustomButton } from '../Buttons/CustomButton';
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import CheckOutlinedIcon from '@mui/icons-material/CheckOutlined';
+import ClearOutlinedIcon from '@mui/icons-material/ClearOutlined';
 import style from '../style.module.scss';
+import { ModalWindow } from './ModalWindow';
 
 export const Column = () => {
   const [open, setOpen] = useState(false);
-
+  const [openModal, setOpenModal] = useState(false);
+  const handleOpen = () => setOpenModal(true);
+  const handleClose = () => setOpenModal(false);
   const isOpen = useCallback(() => {
     setOpen((prevValue) => !prevValue);
   }, []);
 
   return (
     <div className={`${style.column}`}>
-      <div className={`${style.column_header}`}>
-        {!open && (
-          <Tooltip title="Click">
+      {!open && (
+        <div className={`${style.column_header}`}>
+          <Tooltip title="Change title" placement="top">
             <div className={`${style.column_title}`} onClick={isOpen}>
               <p>I`m title/ Click me</p>
             </div>
           </Tooltip>
-        )}
-        {open && (
+          <Tooltip title="Delete column" placement="top">
+            <div>
+              <CustomButton
+                icon={<ClearOutlinedIcon />}
+                itemType="button"
+                className={`${style.columnButton} ${style.columnButton__cancel}`}
+                onClick={isOpen}
+              />
+            </div>
+          </Tooltip>
+        </div>
+      )}
+      {open && (
+        <div className={`${style.column_header}`}>
           <form className={style.column_form}>
             <div className={style.column_buttons}>
               <CustomButton
-                textContent="SUBMIT"
+                icon={<CheckOutlinedIcon />}
                 itemType="submit"
                 className={`${style.columnButton} ${style.columnButton__submit}`}
               />
               <CustomButton
-                textContent="CANCEL"
+                icon={<ClearOutlinedIcon />}
                 itemType="button"
                 className={`${style.columnButton} ${style.columnButton__cancel}`}
                 onClick={isOpen}
@@ -38,16 +55,24 @@ export const Column = () => {
             </div>
             <input className={style.column_input} type="text" />
           </form>
-        )}
-      </div>
+        </div>
+      )}
+
       <CustomButton
         typeof="button"
         textContent="Add task"
         className={`${style.columnButton} ${style.columnButton__icon}`}
         style={{ height: '37px' }}
         icon={<AddCircleOutlineOutlinedIcon className={style.icon__add} />}
+        onClick={handleOpen}
       />
-      <div></div>
+
+      <ModalWindow
+        open={openModal}
+        handleClose={handleClose}
+        placeHolder="Enter a title for this task"
+        nameButton="Add task"
+      />
     </div>
   );
 };
--- src/components/designe/Main/Main.tsx
+++ src/components/designe/Main/Main.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CustomButton } from '../Buttons/CustomButton';
 import style from '../style.module.scss';
 import { Column } from '../Board/Column';
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import { ModalWindow } from '../Board/ModalWindow';
 
 export const Main = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const handleOpen = () => setOpenModal(true);
+  const handleClose = () => setOpenModal(false);
+
   return (
     <div className={`${style.container} ${style.container__medium}  ${style.main}`}>
       <Column />
@@ -25,6 +30,14 @@ export const Main = () => {
           className={`${style.columnButton} ${style.columnButton__icon}`}
           style={{ height: '37px' }}
           icon={<AddCircleOutlineOutlinedIcon className={style.icon__add} />}
+          onClick={handleOpen}
+        />
+        <ModalWindow
+          open={openModal}
+          handleClose={handleClose}
+          placeHolder="Enter a title for the new
+        columns"
+          nameButton="Add column"
         />
       </div>
     </div>
