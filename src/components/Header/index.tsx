2582eb0dc9f08ebb96e1eb06396816b34072cc08
novikova.n_qschool <nadeysha23@gmail.com>
2022-05-21 23:15:40 +0400

    PMA-4 feat(header): add dropdown button for user menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -16,50 +16,26 @@ export const Header = ({ signOut, userName, theme }: HeaderPropsType) => {
 
   return (
     <div className={`${styles.container} ${styles.containerBig}  ${styles.header} ${theme}`}>
-      <button className={styles.btn}>Button</button>
-      <div className={styles.dropdown}>
-        <button className={styles.btn} /* style="border-left:1px solid #0d8bf2" */>
-          {userName}
-        </button>
-        <div className={styles.dropdownContent}>
-          <a href="#">Edit profile</a>
-          <a href="#" onClick={signOut}>
-            Sign Out
-          </a>
+      <div className={styles.headerButtonGroup}>
+        <div className={styles.dropdown}>
+          <div className={styles.headerButton}>
+            <Button variant="outlined" color="inherit">
+              {userName}
+            </Button>
+          </div>
+          <div className={styles.dropdownContent}>
+            <a href="#">Edit profile</a>
+            <a href="#" onClick={signOut}>
+              Sign Out
+            </a>
+          </div>
         </div>
-      </div>
-      <ul className={styles.headerButtonGroup}>
-        <li className={styles.headerButton}>
-          <ul>
-            {userName}
-            <li>
-              <Button
-                variant="outlined"
-                color="inherit"
-                className={styles.button}
-                onClick={signOut}
-              >
-                Sign Out
-              </Button>
-            </li>
-            <li>
-              <Button variant="outlined" color="inherit" className={styles.button}>
-                Edit profile
-              </Button>
-            </li>
-          </ul>
-        </li>
-        <li className={styles.headerButton}>
-          <Button variant="outlined" color="inherit" className={styles.button} onClick={signOut}>
-            Sign Out
-          </Button>
-        </li>
-        <li className={styles.headerButton}>
+        <div className={styles.headerButton}>
           <Button variant="outlined" color="inherit" className={styles.button}>
             Create new board
           </Button>
-        </li>
-      </ul>
+        </div>
+      </div>
       <div className={styles.headerSettingsBlock}>
         <ThemeContext.Consumer>
           {({ changeTheme }) => (
