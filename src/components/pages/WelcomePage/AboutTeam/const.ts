1397d8b55fe2701c2da1c2a7c239c877b5e19b63
Bogdan-VS <70603024+Bogdan-VS@users.noreply.github.com>
2022-05-23 15:26:54 +0300

    PMA-20 Welcome page (part 2) (#28)
    * PMA-20 fix(WelcomePage): add changes to design
    
    * PMA-20 fix(WelcomePage): add changes to design
    
    * PMA-20 feat(WelcomePage): add redirect on error 401
    
    * PMA-20 fix(WelcomePage): fix bags
    
    * PMA-20 feat(WelcomePage): add info to the welcome page
    
    * PMA-20 fix(WelcomePage): fix bag
    
    * PMA-20 fix(WelcomePage): fix bag
    
    * PMA-20 fix(WelcomePage): fix styles
    
    * PMA-20 fix(WelcomePage): fix const file
    
    * PMA-20 refactor(WelcomePage): refactor styles in WelcomePage

--- src/components/pages/WelcomePage/AboutTeam/const.ts
+++ src/components/pages/WelcomePage/AboutTeam/const.ts
@@ -1,19 +1,29 @@
+import img1 from '../../../../images/1.jpg';
+import img2 from '../../../../images/2.jpg';
+import img3 from '../../../../images/3.jpg';
+
 export const teamMembers = [
   {
-    src: '',
+    src: img1,
     name: 'Yevhenii Sharovskyi',
+    role: 'Team leader, Frontend developer',
+    github: 'https://github.com/sharovskiye',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   },
   {
-    src: '',
+    src: img2,
     name: 'Nadezhda Novikova',
+    role: 'Frontend developer',
+    github: 'https://github.com/nnadeysha',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   },
   {
-    src: '',
+    src: img3,
     name: 'Bogdan Samonov',
+    role: 'Frontend developer',
+    github: 'https://github.com/Bogdan-VS',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   },
--- src/components/pages/WelcomePage/index.tsx
+++ src/components/pages/WelcomePage/index.tsx
@@ -1,25 +1,25 @@
 import { AboutCourse } from './AboutCourse';
 import { AboutProject } from './AboutProject';
 import { AboutTeam } from './AboutTeam';
-import { SignIn } from './SignIn';
-import { SignUp } from './SignUp';
+import { SignContainer } from './SignContainer';
 
 import styles from './styles.module.scss';
 
 export const WelcomePage = () => {
   return (
     <div className={styles.wrapper}>
-      <header>
-        <h1 className="title">Project Management System</h1>
-        <div className={styles.regContainer}>
-          <SignIn />
-          <SignUp />
+      <header className={styles.header}>
+        <div className={`${styles.wrapperContainer} ${styles.wrapperHeader}`}>
+          <h1 className={styles.headerTitle}>Project Management System</h1>
+          <SignContainer />
         </div>
       </header>
-      <main>
-        <AboutProject />
-        <AboutTeam />
-        <AboutCourse />
+      <main className={styles.main}>
+        <div className={styles.wrapperContainer}>
+          <AboutProject />
+          <AboutTeam />
+          <AboutCourse />
+        </div>
       </main>
     </div>
   );
