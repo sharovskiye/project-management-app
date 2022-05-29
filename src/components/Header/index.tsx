39b17256317be1027d14b98bbb9bf393b854cada
novikova.n_qschool <nadeysha23@gmail.com>
2022-05-29 20:45:28 +0400

    PMA-19 fix(adaptive): delete unused import

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { useToggle } from '../../utils/CustomHook';
